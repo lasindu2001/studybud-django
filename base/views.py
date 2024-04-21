05a273852169fc87ca9b5171a2b442c39bedb002
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-04-21 17:25:52 +0530

    create and complete topics page UI and functionality

--- base/views.py
+++ base/views.py
@@ -160,3 +160,9 @@ def updateUser(request):
             messages.error(request, 'An error occurred during updating')
     context = {'form': form}
     return render(request, 'base/update-user.html', context)
+
+def topicPage(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+    context = {'topics': topics}
+    return render(request, 'base/topics.html', context)
